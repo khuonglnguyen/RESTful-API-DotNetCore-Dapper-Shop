7ae6e6a1f7e670fbb65878a55b8a6b10ca838532
khuongnguyen0412 <lapankhuongnguyen@gmail.com>
2022-02-10 09:41:05 +0700

    Added Dapper micro-ORM

---
+++ APICoreApp/APICoreApp/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using APICoreApp.Models;
+using Dapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace APICoreApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly string _connectionString;
+        public ProductController(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DbConnectionString");
+        }
+        // GET: api/Product
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get()
+        {
+            using (var conn=new SqlConnection(_connectionString))
+            {
+                if (conn.State==System.Data.ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+                var result = await conn.QueryAsync<Product>("SELECT Id, Sku, DiscountPrice, ImageUrl, CreatedAt, IsActive, ViewCount FROM Products",null,null,null,CommandType.Text);
+                return result;
+            }
+        }
+
+        // GET: api/Product/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Product
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Product/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
